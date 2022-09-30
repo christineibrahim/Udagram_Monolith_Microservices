06f1fab9e20bd5c5f6f01e8950ac77ac6c3dd45b
christineibrahim <53993410+christineibrahim@users.noreply.github.com>
2022-09-30 20:16:35 +0200

    Add files via upload

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgresuser
+export POSTGRES_PASSWORD=postgresuser
+export POSTGRES_HOST=proj3-cdr.cin556icysnj.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::proj3-727690673718
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=helloproj3
+export URL=http://localhost:8100
